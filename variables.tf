08403c9fa716ec21e8157026b308bb78cf9d2aa2
Shaikha <shaikha.saalem@outlook.com>
2025-02-19 20:37:41 +0100

    feat: add vault node
    Signed-off-by: Shaikha <shaikha.saalem@outlook.com>

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "Vault_Node_One" {
+    description = "Vault node one"
+    type = string
+    default = "Vault_Node_One"
+}
+
+variable "Vault_Node_Two" {
+    description = "Vault node two"
+    type = string
+    default = "Vault_Node_Two"
+}
